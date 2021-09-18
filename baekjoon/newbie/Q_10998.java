42e37c4f5383648f3242709bd39e4bf154143071
seongsoo96 <seongsoo.dev@gmail.com>
2021-09-18 09:48:56 +0900

    AxB

---
+++ baekjoon/newbie/Q_10998.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//AxB(10998)
+public class Q_10998 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a * b);
+
+    }
+
+}
