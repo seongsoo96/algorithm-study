2876170c75067c509de86d291fad7fd7d347badd
seongsoo96 <seongsoo.dev@gmail.com>
2021-09-19 09:11:50 +0900

    A/B

---
+++ baekjoon/newbie/Q_1008.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//A/B(1008)
+public class Q_1008 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+
+    }
+
+}
