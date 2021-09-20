88ef6ccd35acc9c01c6fc37a4b809785931bb229
seongsoo96 <seongsoo.dev@gmail.com>
2021-09-20 20:33:45 +0900

    사칙연산

---
+++ baekjoon/newbie/Q_10896.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//사칙연산(10896)
+public class Q_10896 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+
+}
