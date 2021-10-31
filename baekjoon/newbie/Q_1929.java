99f6f38ae6df185af5fc082474814759b776713d
seongsoo96 <seongsoo.dev@gmail.com>
2021-10-31 14:53:18 +0900

    소수 구하기(에라토스테네스의 체)

--- baekjoon/newbie/Q_1929.java
+++ baekjoon/newbie/Q_1929.java
@@ -1,3 +1,4 @@
+import java.beans.beancontext.BeanContext;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -9,30 +10,58 @@ public class Q_1929 {
 
     public static void main(String[] args) throws Exception  {
 
+        //에라토스테네스의 체 알고리즘 이용
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        for(int i=m; i<n+1; i++) {
-            int cnt = 0;
-            if(i == 1) {
+        boolean[] check = new boolean[n+1];
+
+        check[0] = check[1] = true;
+
+        for(int i=2; i<=n; i++) {
+            if(check[i] == true) {
                 continue;
-            } else {
-                for(int j=1; j<i+1; j++) {
-                    if(i%j == 0) {
-                        cnt++;
-                    }
-                }
-                if(cnt == 2) {
-                    bw.write(i + "\n");
-                }
+            }
+            for(int j=i+i; j<=n; j=j+i) {
+                check[j] = true;
+            }
+        }
+        for(int i=m; i<=n; i++) {
+            if(check[i] == false) {
+                bw.write(i + "\n");
             }
         }
         bw.flush();
         bw.close();
-        br.close();
+
+        //시간 초과 풀이
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        int m = Integer.parseInt(st.nextToken());
+//        int n = Integer.parseInt(st.nextToken());
+//
+//        for(int i=m; i<n+1; i++) {
+//            int cnt = 0;
+//            if(i == 1) {
+//                continue;
+//            } else {
+//                for(int j=1; j<i+1; j++) {
+//                    if(i%j == 0) {
+//                        cnt++;
+//                    }
+//                }
+//                if(cnt == 2) {
+//                    bw.write(i + "\n");
+//                }
+//            }
+//        }
+//        bw.flush();
+//        bw.close();
+//        br.close();
 
     }
 }
