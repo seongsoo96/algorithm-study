f62e5e8a0ed66f2796b34e1220e4043f91fb0369
seongsoo96 <seongsoo.dev@gmail.com>
2021-11-10 22:31:32 +0900

    A+B - 8

---
+++ baekjoon/newbie/Q_11022.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//A+B - 8(11022)
+public class Q_11022 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = a+b;
+            sb.append("Case #" + i + ": " + a + " + " + b + " = " + c + "\n");
+        }
+        System.out.println(sb);
+    }
+}
+/*
+    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    5
+    1 1
+    2 3
+    3 4
+    9 8
+    5 2
+
+    출력
+    각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다.
+    x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+    Case #1: 1 + 1 = 2
+    Case #2: 2 + 3 = 5
+    Case #3: 3 + 4 = 7
+    Case #4: 9 + 8 = 17
+    Case #5: 5 + 2 = 7
+*/
