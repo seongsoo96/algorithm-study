1135be7b0ab5352878bf3c27dfb27b86a87e837e
seongsoo96 <seongsoo.dev@gmail.com>
2021-11-13 22:10:24 +0900

    팩토리얼

---
+++ baekjoon/newbie/Q_10872.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//팩토리얼(10872)
+public class Q_10872 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int ans = 1;
+        if(n == 0) {
+            System.out.println(ans);
+        } else {
+            for(int i=1; i<=n; i++) {
+                ans *= i;
+            }
+            System.out.println(ans);
+        }
+    }
+}
+/*
+    0보다 크거나 같은 정수 N이 주어진다.
+    이때, N!을 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+    10
+
+    출력
+    첫째 줄에 N!을 출력한다.
+    3628800
+*/
