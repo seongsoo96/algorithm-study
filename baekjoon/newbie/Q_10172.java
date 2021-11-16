b9aecfeb52ccda70aedd5b2a28b3e7abf0b1ecde
seongsoo96 <seongsoo.dev@gmail.com>
2021-11-17 07:41:03 +0900

    개

---
+++ baekjoon/newbie/Q_10172.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+//개(10172)
+public class Q_10172 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+/*
+    아래 예제와 같이 개를 출력하시오.
+
+    입력
+    없음.
+
+    출력
+    개를 출력한다.
+    |\_/|
+    |q p|   /}
+    ( 0 )"""\
+    |"^"`    |
+    ||_/=\\__|
+*/
