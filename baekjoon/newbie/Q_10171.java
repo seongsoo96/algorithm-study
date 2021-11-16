b82b5dcf878b2edb0946928393ebd0ce16e40ad4
seongsoo96 <seongsoo.dev@gmail.com>
2021-11-16 19:03:13 +0900

    고양이

---
+++ baekjoon/newbie/Q_10171.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+//고양이(10171)
+public class Q_10171 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+/*
+    아래 예제와 같이 고양이를 출력하시오.
+
+    입력
+    없음.
+
+    출력
+    고양이를 출력한다.
+    \    /\
+     )  ( ')
+    (  /  )
+     \(__)|
+*/
