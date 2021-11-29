54177e37822c8ee6547e311bbea87726e8dc80ad
seongsoo96 <seongsoo.dev@gmail.com>
2021-11-29 22:25:29 +0900

    숫자 문자열과 영단어

--- programmers/level1/NumStringAndWord.java
+++ programmers/level1/NumStringAndWord.java
@@ -1,9 +1,9 @@
 //https://programmers.co.kr/learn/courses/30/lessons/81301
+//숫자 문자열과 영단어
 
 class NumStringAndWord {
 
     public int solution(String s) {
-
         String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
@@ -13,4 +13,5 @@ class NumStringAndWord {
 
         return Integer.parseInt(s);
     }
+
 }
