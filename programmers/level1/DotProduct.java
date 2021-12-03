6b8732fa9233e249037d662497a884c08a007b8a
이성수 <seongsoo.dev@gmail.com>
2021-12-03 14:32:30 +0900

    내적

--- programmers/level1/DotProduct.java
+++ programmers/level1/DotProduct.java
@@ -1,22 +1,11 @@
-//https://programmers.co.kr/learn/courses/30/lessons/86051
-//없는 숫자 더하기
-class AddMissingNumbers {
-    public int solution(int[] numbers) {
+//https://programmers.co.kr/learn/courses/30/lessons/70128
+//내
+class DotProduct {
+    public int solution(int[] a, int[] b) {
         int answer = 0;
-        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        for(int i=0; i<numbers.length; i++) {
-            for(int j=0; j<10; j++) {
-                if(numbers[i] == arr[j]) {
-                    arr[j] = 0;
-                }
-            }
+        for(int i=0; i<a.length; i++) {
+            answer += a[i]*b[i];
         }
-
-        for(int i=0; i<10; i++) {
-            answer += arr[i];
-        }
-
         return answer;
     }
 }
