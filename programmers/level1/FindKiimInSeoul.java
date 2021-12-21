3e75ff50d9215954e4005c5f9f0d38f6f10e421d
이성수 <seongsoo.dev@gmail.com>
2021-12-21 17:12:53 +0900

    서울에서 김서방 찾기

---
+++ programmers/level1/FindKiimInSeoul.java
@@ -0,0 +1,14 @@
+//https://programmers.co.kr/learn/courses/30/lessons/12919
+//서울에서 김서방 찾기
+
+class FindKiimInSeoul {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i=0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+            }
+        }
+        return answer;
+    }
+}
