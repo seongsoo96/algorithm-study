830e4e0aab5b3bbb64e1ccb47e8490eb035ceb91
seongsoo96 <seongsoo.dev@gmail.com>
2022-03-05 17:35:51 +0900

    다리를 지나는 트럭

--- programmers/level2/BridgeOnTruck.js
+++ programmers/level2/BridgeOnTruck.js
@@ -1,3 +1,5 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42583
+//다리를 지나는 트럭
 function solution(bridge_length, weight, truck_weights) {
 	let bridge = Array.from({ length: bridge_length }, () => 0);
 	let time = 0;
